942024428f4453ac7376d958b6d4bd12cd0ef3e7
Chaturima M <50954235+mchaturima@users.noreply.github.com>
2020-08-23 21:24:14 +0530

    Updates

--- precache-manifest.d044eee327199ea61c02a149e9a42680.js
+++ precache-manifest.d044eee327199ea61c02a149e9a42680.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b866d1fbf72531f6fc8d3dc28a0b4dc0",
+    "revision": "659edab29d2aa30303e04f032ae51b5d",
     "url": "/Publicis-Sapient/index.html"
   },
   {
-    "revision": "dc932f7cd3bc2ce2b312",
-    "url": "/Publicis-Sapient/static/css/main.f87bd411.chunk.css"
+    "revision": "2749864e9202a0996c02",
+    "url": "/Publicis-Sapient/static/css/main.7dace36f.chunk.css"
   },
   {
     "revision": "bcf3596d0ea31a0ff64a",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Publicis-Sapient/static/js/2.274bcf0e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dc932f7cd3bc2ce2b312",
+    "revision": "2749864e9202a0996c02",
     "url": "/Publicis-Sapient/static/js/main.14b24c88.chunk.js"
   },
   {
