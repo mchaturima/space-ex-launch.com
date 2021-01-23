305a0c9b0c1c2b58b50056899fb19ed5c7f06e78
Chaturima M <50954235+mchaturima@users.noreply.github.com>
2021-01-23 21:06:23 +0530

    first commit

---
+++ readmepublicissapient.txt
@@ -0,0 +1,116 @@
+Simple React JS Project
+
+This Project is a Simple ReactJS Project which demonstrates the following:
+
+1. Creating a component in react.
+2. Fetching an Api data using fetch().
+3. Rendering the fetched data to the root.
+4. Basic Css styling and Html has been used for development.
+
+Live Application URL:
+https://mchaturima.github.io/Publicis-Sapient/
+
+Below are the screenshots of the website developed according to the requirements:
+
+#Requirement.No.1: Display all the launches on page load.
+
+![Screenshot (63)](https://user-images.githubusercontent.com/50954235/91216722-0cf63880-e734-11ea-9a99-f23ce2fd5287.png)
+
+
+#Requirement.No.2: When a filter is selected change the api fetch and display data without page refresh.
+  #Requirement.No.2.1: When a Launch year is selected the following appears:
+
+![Screenshot (65)](https://user-images.githubusercontent.com/50954235/91216924-58a8e200-e734-11ea-9f29-509c9e16afad.png)
+
+ #Requirement.No.2.2: When a successful launch true is selected the following appears:
+
+![Screenshot (57)](https://user-images.githubusercontent.com/50954235/91216237-3febfc80-e733-11ea-817b-db98721c63b9.png)
+
+
+ #Requirement.No.2.3: When a successful landing true is selected the following appears:
+
+![Screenshot (59)](https://user-images.githubusercontent.com/50954235/91216446-98bb9500-e733-11ea-8567-f1586832e533.png)
+
+
+ #Requirement.No.2.4: When a year is selected with success launch true and success land true the following appears:
+
+![Screenshot (61)](https://user-images.githubusercontent.com/50954235/91216542-c7d20680-e733-11ea-9fdc-4e9dc93aad0a.png)
+
+
+#Requirement.No.3: Desktop view with 4 columns:
+
+![Screenshot (63)](https://user-images.githubusercontent.com/50954235/91216722-0cf63880-e734-11ea-9a99-f23ce2fd5287.png)
+
+#Requirement.No.4: Tablet view with two columns:
+
+![Screenshot (55)](https://user-images.githubusercontent.com/50954235/91215928-c8b66880-e732-11ea-9faa-6753ce7147a6.png)
+
+
+#Requirement.No.5: Mobile view with one column:
+
+![Screenshot (51)](https://user-images.githubusercontent.com/50954235/90983295-a386f600-e58a-11ea-8e72-7b0ffd61ee40.png)
+
+![Screenshot (53)](https://user-images.githubusercontent.com/50954235/90983350-0e383180-e58b-11ea-97bd-076bd606a4c7.png)
+
+#Requirement.No.6: All the cards should be of same height.
+
+![Screenshot (63)](https://user-images.githubusercontent.com/50954235/91216722-0cf63880-e734-11ea-9a99-f23ce2fd5287.png)
+
+Unit Test Cases:
+
+1. On page load all the launches have to be displayed.
+   Expected Output: All the launches are supposed to be displayed in 4 columns.
+   Actual Output: All the launches are displayed in 4 columns.
+
+2. If user clicks on any year button without clicking on successful buttons and landing buttons.
+    Expected Output: Launches regarding that particular year have to appear.
+    Actual Output: Launches regarding that particular year are appearing.
+
+3. If user clicks on only successful true without clicking on a launch year button and landing buttons.
+    Expected Output: Launches that are successful true in entire data have to appear.
+    Actual Output: Launches that are successful true in entire data are appearing.
+
+4. If user clicks on only successful false without clicking on a launch year button and landing buttons.
+    Expected Output: Launches that are successful false in entire data have to appear.
+    Actual Output: Launches that are successful false in entire data are appearing.
+
+5. If user clicks on only landing true without clicking on a launch year button and successful buttons.
+    Expected Output: Launches that are landing true in entire data have to appear.
+    Actual Output: Launches that are landing true in entire data are appearing.
+
+6. If user clicks on only landing false without clicking on a launch year button and successful buttons.
+    Expected Output: Launches that are landing falsein entire data have to appear.
+    Actual Output: Launches that are landing false in entire data are appearing.
+
+7. If user selects all the three filters.
+    Expected Output: Launches matching that particular filter have to appear.
+    Actual Output: Launches matching that filter are appearing.
+
+8. If any of the filters are selected, reset filters button appears.
+    Expected Output: If filters are selected, reset filters button should appear.
+    Actual Output: If filters are selected, reset filters button are appearing.
+
+9. If user clicks on reset filters button, filters should be cleared and all the launches should appear.
+    Expected Output: Filters should be cleared and all the launches should appear.
+    Actual Output: Filters are cleared and all the launches are appearing.
+
+
+
+
+Hi Radhika,
+I have completed my assignment with the given requirements. Please check the following links asked for submission.
+Link for my github repository: https://github.com/mchaturima/Publicis-Sapient
+Link for the hosted application: https://mchaturima.github.io/Publicis-Sapient/
+Please contact me back if you are not able to access any of the links.
+Looking forward to a positive response.
+Thanks you
+Regards,Chaturima M7760180019
+
+
+
+
+
+
+
+
+
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -8,7 +8,6 @@ export default class MainPage extends Component {
     state = {
         loading: true,
         launch_year: [],
-        links: [],
         filters: {
             launch_year: '',
             launch_success: '',
@@ -17,9 +16,9 @@ export default class MainPage extends Component {
         radio_buttons: false
     }
 
+    // componentDidMount is use to fetch the api on page load.
     async componentDidMount() {
         var launch_year_arr = [];
-        var links_arr = [];
         var complete_data;
 
         const url1 = "https://api.spaceXdata.com/v3/launches?limit=100";
@@ -32,10 +31,8 @@ export default class MainPage extends Component {
             var filtered_launch_year_array = launch_year_arr.filter(function (item, pos) {
                 return launch_year_arr.indexOf(item) === pos;
             });
-            links_arr.push(data[i].links.mission_patch_small)
             this.setState({
                 launch_year: filtered_launch_year_array,
-                links: links_arr,
                 loading: false,
                 datas: complete_data
 
@@ -43,6 +40,7 @@ export default class MainPage extends Component {
         }
     }
 
+    // addfilter is used to fetch the filter that is clicked and send it to applyFilter to create an api.
     addFilter = (e) => {
         let filter = this.state.filters;
         if (e.target.name === 'launch_year') {
@@ -62,8 +60,11 @@ export default class MainPage extends Component {
             });
         }
         this.applyFilter();
-    }
+        document.getElementById('reset-filter').style.display='block';
+        }
 
+        // applyFilter is used to fetch the clicked filter and append it to the constant url
+        // It fetches the api's according to the filter selected
     applyFilter = () => {
         let url = 'https://api.spaceXdata.com/v3/launches?limit=100';
 
@@ -100,104 +101,123 @@ export default class MainPage extends Component {
 
     clearFilters = () => {
         $('.filter_buttons').prop('checked', false);
-        this.componentDidMount();
+        window.location.reload();
+        document.getElementById('reset-filter').style.display='none';
+
     }
 
     render() {
         return (
-            <div className='container'>
-                <h2>SpacEx Launch programs</h2>
-                <div className="col-one">
-                    <p style={filter_p}><b>Filters</b></p>
-                    <p align="center" style={launch_year_p}>Launch Year</p>
-                    <span style={filter_underline}></span>
-                    {this.state.loading || !this.state.launch_year ? (
-                        <div>Loading...</div>
-                    ) : (
-                            this.state.launch_year.map(year => (
-                                <label key={"key" + year}>
-                                    <input type="radio" name="launch_year" key={"launch_year" + year} value={year} onClick={e => this.addFilter(e)} className="filter_buttons"></input>
-                                    <span style={year_button}>{year}</span>
+            <div className="container" id="spacex">
+                <h1>SpacEx Launch programs</h1>
+                <div className="data-container">
+                    <div className="col-filter float-left">
+                        <div className="filter">
+                            <div className="header">
+                                <div className="col name">
+                                    Filter
+                                </div>
+                                <div className="col">
+                                    <button className="reset-btn" id="reset-filter" onClick={e => this.clearFilters(e)}>Reset Filters</button>
+                                </div>
+                            </div>
+                            <div className="year-filter">
+                                <p align="center" style={launch_year_p}>Launch Year</p>
+                                <span style={filter_underline}></span>
+                                {this.state.loading || !this.state.launch_year ? (
+                                    <div>Loading...</div>
+                                ) : (
+                                        this.state.launch_year.map(year => (
+                                            <label key={"key" + year}>
+                                                <input type="radio" name="launch_year" key={"launch_year" + year} value={year} onClick={e => this.addFilter(e)} className="filter_buttons"></input>
+                                                <span style={year_button}>{year}</span>
+                                            </label>
+                                        ))
+                                    )
+                                }
+                            </div>
+                            <div className="launch-filter">
+                                <p align="center" style={successful_launch_p}>Successful Launch</p>
+                                <span style={filter_underline}></span>
+                                <label>
+                                    <input type="radio" name="launch_success" value="true" onClick={e => this.addFilter(e)} className="filter_buttons"></input>
+                                    <span style={year_button}>True</span>
                                 </label>
-                            ))
-                        )
-                    }
-                    <p align="center" style={successful_launch_p}>Successful Launch</p>
-                    <span style={filter_underline}></span>
-                    <label>
-                        <input type="radio" name="launch_success" value="true" onClick={e => this.addFilter(e)} className="filter_buttons"></input>
-                        <span style={year_button}>True</span>
-                    </label>
-                    <label>
-                        <input type="radio" name="launch_success" value="false" onClick={e => this.addFilter(e)} className="filter_buttons"></input>
-                        <span style={year_button}>False</span>
-                    </label>
-                    <p align="center" style={successful_Laing_p}>Successful Landing</p>
-                    <span style={filter_underline}></span>
-                    <label>
-                        <input type="radio" name="land_success" value="true" onClick={e => this.addFilter(e)} className="filter_buttons"></input>
-                        <span style={year_button}>True</span>
-                    </label>
-                    <label>
-                        <input type="radio" name="land_success" value="false" onClick={e => this.addFilter(e)} className="filter_buttons"></input>
-                        <span style={year_button}>False</span>
-                    </label>
-                    <button className="reset-filter-btn" onClick={e => this.clearFilters(e)}>Reset Filters</button>
+                                <label>
+                                    <input type="radio" name="launch_success" value="false" onClick={e => this.addFilter(e)} className="filter_buttons"></input>
+                                    <span style={year_button}>False</span>
+                                </label>
+                            </div>
+                            <div className="land-filter">
+                                <p align="center" style={successful_Laing_p}>Successful Landing</p>
+                                <span style={filter_underline}></span>
+                                <label>
+                                    <input type="radio" name="land_success" value="true" onClick={e => this.addFilter(e)} className="filter_buttons"></input>
+                                    <span style={year_button}>True</span>
+                                </label>
+                                <label>
+                                    <input type="radio" name="land_success" value="false" onClick={e => this.addFilter(e)} className="filter_buttons"></input>
+                                    <span style={year_button}>False</span>
+                            </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-data float-left">
+                        <div className="data">
+                            {this.state.loading || !this.state.datas ? (
+                                <div>Loading...</div>
+                            ) : (this.state.datas.length > 0) ?
+                                    this.state.datas.map(function (data, index) {
+                                        // console.log(this.state.data)
+                                        return (
+                                            <div className="card float-left" key={index}>
+                                                <div key={index} className="card-details">
+                                                <img src={data.links.mission_patch_small} alt="alt_image" style={images}></img>
+                                                <p style={mission_name}>{data.mission_name}</p>
+                                                <p style={mission_ids}><b>Mission Ids:</b></p>
+
+                                                {data.mission_id.length === 0 ? (
+                                                    <li style={mission_ids}>None</li>
+                                                ) : (
+                                                        data.mission_id.map(function (id, index) {
+                                                            return (
+                                                                <li style={mission_ids} key={index}>{id}</li>
+
+                                                            )
+                                                        })
+                                                    )}
+                                                <p style={mission_ids}><b>Launch Year:</b>{data.launch_year}</p>
+                                                {data.launch_success == null ? (
+                                                    <p style={mission_ids}><b>Successful Launch:</b> Details not available</p>
+                                                ) : data.launch_success === true ? (
+                                                    <p style={mission_ids}><b>Successful Launch:</b> True</p>
+                                                ) : (
+                                                            <p style={mission_ids}><b>Successful Launch:</b> False</p>
+
+                                                        )}
+                                                {data.rocket.first_stage.cores[0].land_success == null ? (
+                                                    <p style={mission_ids}><b>Successful Landing:</b> Details not available</p>
+                                                ) : data.rocket.first_stage.cores[0].land_success === true ? (
+                                                    <p style={mission_ids}><b>Successful Landing:</b> True</p>
+                                                ) : (
+                                                            <p style={mission_ids}><b>Successful Landing:</b> False</p>
+                                                        )}
+
+                                            </div>
+                                            </div>
+                                        )
+                                    }) : <div>No data available for the applied filter</div>
+                            }
+                        </div>
+                    </div>
                 </div>
-                <div className="col-two" id="col-two-container">
-                    {this.state.loading || !this.state.links ? (
-                        <div>Loading...</div>
-                    ) : (this.state.datas.length > 0) ?
-                            this.state.datas.map(function (data, index) {
-                                // console.log(this.state.data)
-                                return (
-                                    <div style={image_card} key={index} className="image_card">
-                                        <img src={data.links.mission_patch_small} alt="alt_image" style={images}></img>
-                                        <p style={mission_name}>{data.mission_name}</p>
-                                        <p style={mission_ids}><b>Mission Ids:</b></p>
-
-                                        {data.mission_id.length === 0 ? (
-                                            <li style={mission_ids}>None</li>
-                                        ) : (
-                                                data.mission_id.map(function (id, index) {
-                                                    return (
-                                                        <li style={mission_ids} key={index}>{id}</li>
-
-                                                    )
-                                                })
-                                            )}
-                                        <p style={mission_ids}><b>Launch Year:</b>{data.launch_year}</p>
-                                        {data.launch_success == null ? (
-                                            <p style={mission_ids}><b>Successful Launch:</b> Details not available</p>
-                                        ) : data.launch_success === true ? (
-                                            <p style={mission_ids}><b>Successful Launch:</b> True</p>
-                                        ) : (
-                                                    <p style={mission_ids}><b>Successful Launch:</b> False</p>
-
-                                                )}
-                                        {data.rocket.first_stage.cores[0].land_success == null ? (
-                                            <p style={mission_ids}><b>Successful Landing:</b> Details not available</p>
-                                        ) : data.rocket.first_stage.cores[0].land_success === true ? (
-                                            <p style={mission_ids}><b>Successful Landing:</b> True</p>
-                                        ) : (
-                                                    <p style={mission_ids}><b>Successful Landing:</b> False</p>
-                                                )}
-
-                                    </div>
-                                )
-                            }) : <div>No data available for the applied filter</div>
-                    }
-                </div>
+                <div className="dev-details">Developed by: Chaturima M</div>
             </div>
         )
     }
 }
 
-let filter_p = {
-    "padding": "0px",
-    "margin": "0px",
-    "textAlign": "left"
-}
+
 
 let launch_year_p = {
     "padding": "0px",
@@ -233,17 +253,11 @@ let filter_underline = {
     "width": "50%",
     "margin": "auto"
 }
-let image_card = {
-    // "margin": "5px",
-    // "float": "left",
-    // "backgroundColor": "#fff",
-    // "height": "540px",
-    // "width": "300px"
-}
 
 let images = {
-    "height": "200px",
-    "width": "200px",
+    // "height": "200px",
+    // "width": "200px",
+    "width":"60%",
     "backgroundColor": "#f2f2f2",
     "margin": "20px",
     "marginBottom": "0px"
@@ -262,4 +276,5 @@ let mission_ids = {
     "marginLeft": "22px",
     "padding": "0px"
 }
+
 
